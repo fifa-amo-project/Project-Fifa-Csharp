0f5b20950102c2dd1d41deb571f367fde12ca7a0
Unknown <Jaibreyon.lourens@gmail.com>
2019-04-15 10:44:08 +0200

    Applicatie Layout

---
+++ FifaGokApp/FifaGokApp/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace FifaGokApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.firstTeamTextBox = new System.Windows.Forms.TextBox();
+            this.sixtTeamTextBox = new System.Windows.Forms.TextBox();
+            this.fourthTeamTextBox = new System.Windows.Forms.TextBox();
+            this.secondTeamTextBox = new System.Windows.Forms.TextBox();
+            this.fifthTeamTextBox = new System.Windows.Forms.TextBox();
+            this.thirdTeamTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.thirdTeamTextBox);
+            this.groupBox1.Controls.Add(this.fifthTeamTextBox);
+            this.groupBox1.Controls.Add(this.secondTeamTextBox);
+            this.groupBox1.Controls.Add(this.fourthTeamTextBox);
+            this.groupBox1.Controls.Add(this.sixtTeamTextBox);
+            this.groupBox1.Controls.Add(this.firstTeamTextBox);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(214, 173);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(372, 314);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.Location = new System.Drawing.Point(193, 9);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(451, 39);
+            this.welcomeLabel.TabIndex = 0;
+            this.welcomeLabel.Text = "Welkom in de FIFA gok app!\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(194, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 66);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Zet een bod op jou team \r\nof een ander team naar keuze.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(227, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(227, 51);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "label7";
+            // 
+            // firstTeamTextBox
+            // 
+            this.firstTeamTextBox.Location = new System.Drawing.Point(47, 27);
+            this.firstTeamTextBox.Name = "firstTeamTextBox";
+            this.firstTeamTextBox.Size = new System.Drawing.Size(55, 20);
+            this.firstTeamTextBox.TabIndex = 6;
+            // 
+            // sixtTeamTextBox
+            // 
+            this.sixtTeamTextBox.Location = new System.Drawing.Point(159, 84);
+            this.sixtTeamTextBox.Name = "sixtTeamTextBox";
+            this.sixtTeamTextBox.Size = new System.Drawing.Size(55, 20);
+            this.sixtTeamTextBox.TabIndex = 7;
+            // 
+            // fourthTeamTextBox
+            // 
+            this.fourthTeamTextBox.Location = new System.Drawing.Point(159, 53);
+            this.fourthTeamTextBox.Name = "fourthTeamTextBox";
+            this.fourthTeamTextBox.Size = new System.Drawing.Size(55, 20);
+            this.fourthTeamTextBox.TabIndex = 8;
+            // 
+            // secondTeamTextBox
+            // 
+            this.secondTeamTextBox.Location = new System.Drawing.Point(159, 24);
+            this.secondTeamTextBox.Name = "secondTeamTextBox";
+            this.secondTeamTextBox.Size = new System.Drawing.Size(55, 20);
+            this.secondTeamTextBox.TabIndex = 9;
+            // 
+            // fifthTeamTextBox
+            // 
+            this.fifthTeamTextBox.Location = new System.Drawing.Point(47, 79);
+            this.fifthTeamTextBox.Name = "fifthTeamTextBox";
+            this.fifthTeamTextBox.Size = new System.Drawing.Size(55, 20);
+            this.fifthTeamTextBox.TabIndex = 10;
+            // 
+            // thirdTeamTextBox
+            // 
+            this.thirdTeamTextBox.Location = new System.Drawing.Point(47, 53);
+            this.thirdTeamTextBox.Name = "thirdTeamTextBox";
+            this.thirdTeamTextBox.Size = new System.Drawing.Size(55, 20);
+            this.thirdTeamTextBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 666);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.welcomeLabel);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox thirdTeamTextBox;
+        private System.Windows.Forms.TextBox fifthTeamTextBox;
+        private System.Windows.Forms.TextBox secondTeamTextBox;
+        private System.Windows.Forms.TextBox fourthTeamTextBox;
+        private System.Windows.Forms.TextBox sixtTeamTextBox;
+        private System.Windows.Forms.TextBox firstTeamTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
